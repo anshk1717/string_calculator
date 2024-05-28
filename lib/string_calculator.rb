0a0cc0b368cb8c0c076e7cefd59209aa3a4dc15c
ashish chouhan <anshk1717@gmail.com>
2024-05-28 14:57:48 +0530

    Implement code for handling custom delimiters

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,6 +2,11 @@ class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
     return numbers.to_i if numbers.to_i.to_s == numbers
-    numbers.split(/[\n,]/).map(&:to_i).reduce(:+)
+    if numbers.start_with?("//")
+      delimiter, numbers = numbers[2..].split("\n", 2)
+    else
+      delimiter = /[\n,]/
+    end
+    numbers.split(delimiter).map(&:to_i).reduce(:+)
   end
 end
